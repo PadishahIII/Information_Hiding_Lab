7e82818f2ca89edb7b2c7d5c2ed1049b97ac9007
PadishahIII <350717997@qq.com>
2022-03-28 18:55:43 +0800

    更新为灰度图像

--- rs_lsb_256.py
+++ rs_lsb_256.py
@@ -39,185 +39,41 @@ def BittoString(bitstr):
         for j in range(i))
 
 
-def encode(path, new_path, data):
-    im = Image.open(path)
-    data = StringtoBit(data)
-    width = im.size[0]
-    height = im.size[1]
-    if (width * height < len(data)):
-        print("Your string is too long!")
-        return False
-    count = 0
-    for w in range(width):  # 纵向嵌入
-        for h in range(height):
-            pixel = im.getpixel((w, h))
-            R_byte = pixel[0]
-            G_byte = pixel[1]
-            B_byte = pixel[2]
-            R_bit = bin(R_byte).replace('0b', '')
-            R_bit = ReplaceLastBit(R_bit, data[count])
-            im.putpixel((w, h), (int(R_bit, 2), G_byte, B_byte))
-            count += 1
-            if (count >= len(data)):  #已经嵌入所有信息
-                im.save(new_path)
-                return True
-    im.save(new_path)
-    return True
-
-
-# random_rate:随机率，每八个bit中随机比特的个数
-# 除了随机的比特，其它比特都为1
-def encode_random(path, new_path, random_rate):
-    im = Image.open(path)
-    width = im.size[0]
-    height = im.size[1]
-    data = ''
-    for i in range(0, width * height, 8):
-        for ii in range(i, i + random_rate):
-            data += '0' if random() > 0.5 else '1'
-        for ii in range(i + random_rate, i + 8):
-            data += '1'
-    count = 0
-    for w in range(width):  # 纵向嵌入
-        for h in range(height):
-            pixel = im.getpixel((w, h))
-            R_byte = pixel[0]
-            G_byte = pixel[1]
-            B_byte = pixel[2]
-            if (R_byte % 2 == 0):
-                if (data[count] == '1'):
-                    R_byte += 1
-            else:
-                if (data[count] == '0'):
-                    R_byte -= 1
-            im.putpixel((w, h), (R_byte, G_byte, B_byte))
-            count += 1
-    im.save(new_path)
-    return True
-
-
-# random_rate:随机率，嵌入秘密信息的像素数目所占比例
-# 除了随机的比特，其它比特都为1
-def encode_random_2(path, new_path, random_rate):
-    im = Image.open(path)
-    width = im.size[0]
-    height = im.size[1]
-    max_len = width * height * random_rate
-    data = ''
-    for i in range(0, max_len):
-        data += '0' if random() > 0.5 else '1'
-    count = 0
-    for w in range(width):  # 纵向嵌入
-        for h in range(height):
-            pixel = im.getpixel((w, h))
-            R_byte = pixel[0]
-            G_byte = pixel[1]
-            B_byte = pixel[2]
-            if (R_byte % 2 == 0):
-                if (data[count] == '1'):
-                    R_byte += 1
-            else:
-                if (data[count] == '0'):
-                    R_byte -= 1
-            im.putpixel((w, h), (R_byte, G_byte, B_byte))
-            count += 1
-            if (count > max_len):
-                im.save(new_path)
-                return True
-    im.save(new_path)
-    return True
-
-    # random_rate:随机率，嵌入秘密信息的像素数目所占比例
-
-
-# 除了随机的比特，其它比特都为1
-def encode_random_3(path, new_path, random_rate):
-    im = Image.open(path)
-    width = im.size[0]
-    height = im.size[1]
-    max_len = width * height
-    data = ''
-    for i in range(0, max_len):
-        data += '0' if random() > 0.5 else '1'
-    pos = ''
-    for i in range(0, max_len):
-        pos += '1' if int(1e8 * random()) % rate_list[random_rate -
-                                                      1] == 0 else '0'
-    count = 0
-    for w in range(width):  # 纵向嵌入
-        for h in range(height):
-            pixel = im.getpixel((w, h))
-            R_byte = pixel[0]
-            G_byte = pixel[1]
-            B_byte = pixel[2]
-            if (pos[count] == '1'):
-                if (R_byte % 2 == 0):
-                    if (data[count] == '1'):
-                        R_byte += 1
-                else:
-                    if (data[count] == '0'):
-                        R_byte -= 1
-            im.putpixel((w, h), (R_byte, G_byte, B_byte))
-            count += 1
-            if (count > max_len):
-                im.save(new_path)
-                return True
-    im.save(new_path)
-    return True
-
-
-def decode(path):
-    im = Image.open(path)
-    width = im.size[0]
-    height = im.size[1]
-
-    data = ''.join(
-        bin(im.getpixel((w, h))[0])[-1] for w in range(width)
-        for h in range(height))
-    print(BittoString(data))
-
-
 # 非负翻转
 def Non_negative_evert(im_old):
-    im = Image.new(mode="RGB", size=[im_old.size[0], im_old.size[1]])
+    im = Image.new(mode="L", size=[im_old.size[0], im_old.size[1]])
     width = im_old.size[0]
     height = im_old.size[1]
     for w in range(width):
         for h in range(height):
             pixel = im_old.getpixel((w, h))
-            R_byte = pixel[0]
-            G_byte = pixel[1]
-            B_byte = pixel[2]
             rand_bit = 0 if random() > 0.5 else 1
-            if (R_byte % 2 == 0):
+            if (pixel % 2 == 0):
                 if (rand_bit == 1):
-                    R_byte += 1
+                    pixel += 1
             else:
                 if (rand_bit == 0):
-                    R_byte -= 1
-            im.putpixel((w, h), (R_byte, G_byte, B_byte))
+                    pixel -= 1
+            im.putpixel((w, h), pixel)
     return im
 
 
 # 非正翻转
 def Non_positive_evert(im_old):
-    im = Image.new(mode="RGB", size=[im_old.size[0], im_old.size[1]])
+    im = Image.new(mode="L", size=[im_old.size[0], im_old.size[1]])
     width = im_old.size[0]
     height = im_old.size[1]
     for w in range(width):
         for h in range(height):
             pixel = im_old.getpixel((w, h))
-            R_byte = pixel[0]
-            G_byte = pixel[1]
-            B_byte = pixel[2]
             rand_bit = 0 if random() > 0.5 else 1
-            if (R_byte % 2 == 0):
+            if (pixel % 2 == 0):
                 if (rand_bit == 1):
-                    R_byte = (R_byte - 1) if R_byte != 0 else 0  #TODO
+                    pixel = (pixel - 1) if pixel != 0 else 0  #TODO
             else:
                 if (rand_bit == 0):
-                    R_byte += 1
-            im.putpixel((w, h), (R_byte, G_byte, B_byte))
+                    pixel += 1
+            im.putpixel((w, h), pixel)
     return im
 
 
@@ -281,7 +137,7 @@ def Correlation_list(im):
             data = [[], [], [], [], [], [], [], []]
             for jj in range(8):
                 for ii in range(8):
-                    data[ii] += [im.getpixel((8 * i + ii, 8 * j + jj))[0]]
+                    data[ii] += [im.getpixel((8 * i + ii, 8 * j + jj))]
             correlation_list += [Correlation(data)]
     return correlation_list
 
@@ -319,48 +175,6 @@ def RS(im):
     return Rm, Rm_, Sm, Sm_
 
 
-def LSB():
-    path = input("Input the path of image:")
-    while (os.path.exists(path) == False):
-        path = input(path + " does not exists , please input again:")
-    new_path = input("Input save path:")
-    data = input("Input message to insert:")
-    #file = open("txt", "r")
-    #data = file.read()
-    while (encode(path, new_path, data) == False):
-        data = input("Input message:")
-    print("New image saved to:" + new_path)
-    print("Decode :")
-    decode(new_path)
-
-
-def LSB_random():
-    path = "raw.bmp"
-    random_rate = int(input("Input random rate:(0-8)"))
-    while (random_rate not in range(0, 9)):
-        random_rate = input("Invail,input again:(0-8)")
-    new_path = "random" + str(random_rate) + ".bmp"
-    encode_random(path, new_path, random_rate)
-
-
-def LSB_random_2():
-    path = "1.bmp"
-    random_rate = int(input("Input random rate:(0-8)"))
-    while (random_rate not in range(0, 9)):
-        random_rate = input("Invail,input again:(0-8)")
-    new_path = "random_" + str(random_rate) + ".bmp"
-    encode_random_2(path, new_path, random_rate)
-
-
-def LSB_random_3():
-    path = "1.bmp"
-    random_rate = int(input("Input random rate:(1-8)"))
-    while (random_rate not in range(random_bit_bottom, random_bit_top)):
-        random_rate = input("Invail,input again:(1-8)")
-    new_path = "random_3_" + str(random_rate) + ".bmp"
-    encode_random_3(path, new_path, random_rate)
-
-
 def _RS_():
     path1 = input("RS Analyze\nInput path1:")
     while (os.path.exists(path1) == False):
@@ -381,8 +195,8 @@ def RS_show():
     Rm_ = []
     Sm = []
     Sm_ = []
-    for i in range(0, 9):
-        path = 'random' + str(i) + '.bmp'
+    for i in range(0, 6):
+        path = str(int(pow(2, i))) + '.bmp'
         print("******************\n" + path)
         rm, rm_, sm, sm_ = RS(Image.open(path))
         Rm += [rm]
@@ -390,8 +204,8 @@ def RS_show():
         Sm += [sm]
         Sm_ += [sm_]
     random_list = []
-    for i in range(0, 9):
-        random_list += [str(i / 8)[0:5]]
+    for i in range(0, 6):
+        random_list += [str(1 / pow(2, i))[0:5]]
     plt.figure(figsize=(20, 10), dpi=100)
     plt.plot(random_list, Rm, c='red', linestyle='--', label='R+')
     plt.plot(random_list, Rm_, c='red', label='R-')
@@ -406,22 +220,22 @@ def RS_show_():
     Rm_ = []
     Sm = []
     Sm_ = []
-    for i in range(random_bit_bottom, random_bit_top):
-        path = 'random_3_' + str(i) + '.bmp'
+    for i in range(0, 6):
+        path = str(int(pow(2, i))) + '.bmp'
         print("******************\n" + path)
         rm, rm_, sm, sm_ = RS(Image.open(path))
         Rm += [rm]
         Rm_ += [rm_]
         Sm += [sm]
         Sm_ += [sm_]
-    rm, rm_, sm, sm_ = RS(Image.open("1.bmp"))  #原图像
+    rm, rm_, sm, sm_ = RS(Image.open("lena_gray.bmp"))  #原图像
     Rm += [rm]
     Rm_ += [rm_]
     Sm += [sm]
     Sm_ += [sm_]
     random_list = []
-    for i in range(random_bit_bottom, random_bit_top):
-        random_list += [str(1 / rate_list[i - 1])[0:5]]
+    for i in range(0, 6):
+        random_list += [str(1 / pow(2, i))[0:5]]
     random_list.reverse()
     random_list[:0] = [0]
     Rm.reverse()
@@ -442,11 +256,11 @@ if __name__ == "__main__":
     #for i in range(512 * 512 // 8):
     #    file.write('~')
     #file.close()
-    LSB()  #用户输入秘密信息
+    #LSB()  #用户输入秘密信息
     #while (True):
     #LSB_random()  #第一种方式的随机嵌入
     #LSB_random_2()  #第二种方式的随机嵌入
     #LSB_random_3()
     #_RS_()  #比较两张图像的Rm
     #RS_show()  #使用随机嵌入的图像
-    #RS_show_()
+    RS_show_()
